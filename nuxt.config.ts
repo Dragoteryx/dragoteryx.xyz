eb3bc95a3078169e7e1d953d0299d8b09595e411
Dragoteryx <dragoteryx@live.fr>
2026-03-24 13:11:28 +0100

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import wasm from "vite-plugin-wasm";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/color-mode", "@nuxtjs/mdc", "@nuxt/icon"],
+	modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/color-mode", "@nuxtjs/mdc", "@nuxt/icon", "@nuxtjs/tailwindcss"],
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: true },
 	vite: {
